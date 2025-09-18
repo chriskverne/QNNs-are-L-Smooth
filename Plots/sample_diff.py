953206d9dea731cccc62a97805785ee387e71fff
Christopher Kverne <chris.kverne@gmail.com>
2025-09-18 01:20:35 -0400

    added more data

--- Plots/sample_diff.py
+++ Plots/sample_diff.py
@@ -1,14 +1,15 @@
-4q 1L 3g
-Largest Hessian Norm Sample 0: 0.4999062094294668
-Largest Hessian Norm Sample 1: 0.49934905537993274
-Largest Hessian Norm Sample 2: 0.4995161097763148
-Largest Hessian Norm Sample 3: 0.4997857843612706
-Largest Hessian Norm Sample 4: 0.4999155270305704
-Largest Hessian Norm Sample 5: 0.49975204244492194
-Largest Hessian Norm Sample 6: 0.4996839043760176
-Largest Hessian Norm Sample 7: 0.4999595323163457
-Largest Hessian Norm Sample 8: 0.49933945567253324
-Largest Hessian Norm Sample 9: 0.49953634219434545
+import numpy as np
+# 4q 1L 3g
+# Largest Hessian Norm Sample 0: 0.4999062094294668
+# Largest Hessian Norm Sample 1: 0.49934905537993274
+# Largest Hessian Norm Sample 2: 0.4995161097763148
+# Largest Hessian Norm Sample 3: 0.4997857843612706
+# Largest Hessian Norm Sample 4: 0.4999155270305704
+# Largest Hessian Norm Sample 5: 0.49975204244492194
+# Largest Hessian Norm Sample 6: 0.4996839043760176
+# Largest Hessian Norm Sample 7: 0.4999595323163457
+# Largest Hessian Norm Sample 8: 0.49933945567253324
+# Largest Hessian Norm Sample 9: 0.49953634219434545
 
 x1 = [
     0.4999062094294668,
@@ -23,12 +24,12 @@ x1 = [
     0.49953634219434545
 ]
 
-4q 5L 3g s=250
-Largest Hessian Norm Sample 0: 1.8523511746256853
-Largest Hessian Norm Sample 1: 1.8498826725573956
-Largest Hessian Norm Sample 2: 1.7666526803083482
-Largest Hessian Norm Sample 3: 1.781313599631199
-Largest Hessian Norm Sample 4: 1.7561211072400749
+# 4q 5L 3g s=250
+# Largest Hessian Norm Sample 0: 1.8523511746256853
+# Largest Hessian Norm Sample 1: 1.8498826725573956
+# Largest Hessian Norm Sample 2: 1.7666526803083482
+# Largest Hessian Norm Sample 3: 1.781313599631199
+# Largest Hessian Norm Sample 4: 1.7561211072400749
 
 # 4q 10L 3g s=250
 # Largest Hessian Norm Sample 0: 3.1503145477382013
@@ -58,7 +59,7 @@ Largest Hessian Norm Sample 4: 1.7561211072400749
 # Largest Hessian Norm Sample 0: 3.18747560445007
 # Largest Hessian Norm Sample 0: 3.169763958057894
 
-x = [
+x3 = [
     3.1503145477382013,
     3.07422778104954,
     3.1528630855574176,
@@ -84,12 +85,31 @@ x = [
     3.1874913216158403,
     3.1343787639263807,
     3.18747560445007,
-    3.169763958057894
-]
+    3.169763958057894,
+    3.16318963237763,
+    3.170995775509595,
+    3.1249391956844694,
+    3.184021752271728,
+    3.173027788556377,
+    3.062623370337259,
+    3.240254788025804,
+    3.0876927452201706,
+    3.1795931785628633,
+    3.17201460519422,
+    3.124478140012997,
+    3.089701607741366,
+3.1886651269325093,
+3.1581377839291087,
+3.2298756547160026,
+3.129455677944446,
+3.1202053488849386,
+3.1977490031658693,
+3.215683453542765,
+3.094196612094036]
 
+X3 = [max(x3[i:i+5]) for i in range(0, len(x3), 5)]
 
-
-
+print(X3)
 
 
 
